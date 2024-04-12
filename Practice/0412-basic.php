89e947b3be2f8acb0a11448f3cf74299b4c89ec8
Jin <jinchang0501@gmail.com>
2024-04-12 11:41:53 +0800

    php practice

---
+++ Practice/0412-basic.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "我的標頭" ?></title>
+</head>
+
+<body>
+    <?php echo 123 + 123 ?>
+</body>
+
+</html>
