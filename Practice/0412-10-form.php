ab84f690c7f86040baf7f495d81dbca1bc1d919a
Jin <jinchang0501@gmail.com>
2024-04-12 16:41:45 +0800

    php class

---
+++ Practice/0412-10-form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+        <input type="text" name="account" placeholder="帳號"><br>
+        <input type="password" name="password" placeholder="密碼"><br>
+        <button>送出</button>
+    </form>
+</body>
+
+</html>
---
+++ Practice/0412-11-form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="./0412-09-query string_1.php" method="get">
+        <input type="number" name="a" placeholder="a"><br>
+        <input type="number" name="b" placeholder="b"><br>
+        <button>送出</button>
+    </form>
+</body>
+
+</html>
