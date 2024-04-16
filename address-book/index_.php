1979a728c93ea4d286b40b7edb5c4dcb61d458b1
Jin <jinchang0501@gmail.com>
2024-04-16 16:50:30 +0800

    0416

---
+++ address-book/index_.php
@@ -0,0 +1,8 @@
+<?php include __DIR__ . "/part/html-header.php"; ?>
+<?php include __DIR__ . "/part/navbar.php"; ?>
+
+<div class="container">
+    <h1>Home</h1>
+</div>
+<?php include __DIR__ . "/part/scripts.php"; ?>
+<?php include __DIR__ . "/part/html-footer.php"; ?>
--- address-book/list.php
+++ address-book/list.php
@@ -4,12 +4,102 @@ require __DIR__ . './../config/pdo-connect.php';
 
 $per_page = 20; #每頁有幾筆
 
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+
+if ($page < 1) {
+    header('Location: ?page=1');
+    exit;
+}
 
 #總筆數
-$t_sql = "SELECT COUNT(*) FROM address_book";
+$t_sql = "SELECT COUNT(sid) FROM address_book";
 
 $totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
 
+# 總頁數
 $totalPages = ceil($totalRows / $per_page);
+if ($page > $totalPages) {
+    header("Location: ?page={$totalPages}");
+    exit; # 結束這支程式
+}
 
-echo json_encode([$totalRows, $totalPages]);
+
+// SELECT * FROM `address_book` ORDER BY sid DESC LIMIT 0, 20
+// SELECT * FROM `address_book` ORDER BY sid DESC LIMIT 20, 20
+// SELECT * FROM `address_book` ORDER BY sid DESC LIMIT 40, 20
+// SELECT * FROM `address_book` ORDER BY sid DESC LIMIT 60, 20
+
+
+
+$sql = sprintf(
+    "SELECT * FROM address_book order by sid DESC LIMIT %s,%s",
+    ($page - 1) * $per_page,
+    $per_page
+);
+
+$rows = $pdo->query($sql)->fetchAll();
+
+// echo json_encode([
+//     'totalRows' => $totalRows,
+//     'totalPages' => $totalPages,
+//     'rows' => $rows,
+// ]);
+
+
+include __DIR__ . "/part/html-header.php";
+include __DIR__ . "/part/navbar.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <?php for ($i = 1; $i <= 10; $i++) : ?>
+                        <li class="page-item">
+                            <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+                        </li>
+                    <?php endfor ?>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">mobile</th>
+                        <th scope="col">birthday</th>
+                        <th scope="col">address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($rows as $r) : ?>
+                        <tr>
+                            <td><?= $r['sid'] ?></td>
+                            <td><?= $r['name'] ?></td>
+                            <td><?= $r['email'] ?></td>
+                            <td><?= $r['mobile'] ?></td>
+                            <td><?= $r['birthday'] ?></td>
+                            <td><?= $r['address'] ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<?php include __DIR__ . "/part/scripts.php"; ?>
+<?php include __DIR__ . "/part/html-footer.php"; ?>
